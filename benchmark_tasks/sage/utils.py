fce6ed635d4d1c3e84329c1d0421619e0ea0f23a
Alex-ast7 <81142538+Alex-ast7@users.noreply.github.com>
2026-03-02 16:23:28 +0300

    add filter

--- benchmark_tasks/sage/utils.py
+++ benchmark_tasks/sage/utils.py
@@ -1,4 +1,6 @@
 from typing import Dict, List
+from lm_eval.api.filter import Filter
+from lm_eval.api.registry import register_filter
 import warnings
 
 try:
@@ -67,3 +69,21 @@ def process_results(doc: Dict, results: List[str]) -> Dict[str, float]:
         "SPELL_F1": spell_f1,
         "PUNCT_F1": punct_f1
     }
+
+@register_filter("remove_whitespace_and_nones")
+class RemoveWhitespaceAndNones(Filter):
+
+    def apply(self, resps: list[list[str]], docs: list[dict]) -> list[list[str]]:
+        def filter_set(inst):
+            filtered_resp = []
+            for resp in inst:
+                if not resp:
+                    resp = ""
+                else:
+                    resp = resp.lstrip()
+                filtered_resp.append(resp)
+            return filtered_resp
+
+        filtered_resps = [filter_set(resp) for resp in resps]
+
+        return filtered_resps
