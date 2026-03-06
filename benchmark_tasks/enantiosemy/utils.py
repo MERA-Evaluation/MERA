e461b0afa9700d01eeae431fc5ffbe970334912d
Alex-ast7 <81142538+Alex-ast7@users.noreply.github.com>
2026-03-06 11:31:12 +0300

    change answer format

--- benchmark_tasks/enantiosemy/utils.py
+++ benchmark_tasks/enantiosemy/utils.py
@@ -1,20 +1,12 @@
 from typing import Dict, List, Any
+from lm_eval.api.filter import Filter
+from lm_eval.api.registry import register_filter
 from transformers.data.metrics import squad_metrics
 
 
-
-
 def doc_to_text(doc: Dict[str, Any]) -> str:
-    inputs = doc["inputs"]
 
-    return (
-        "{task}\n\n{text}\n\n"
-        "А) {option_a}\n"
-        "Б) {option_b}\n"
-        "В) {option_c}\n"
-        "Г) {option_d}\n\n"
-        "Ответ:"
-    ).format(**inputs)
+    return doc["instruction"].format(**doc["inputs"])
 
 
 def process_results(doc: Dict, results: List[str]) -> Dict:
@@ -25,5 +17,23 @@ def process_results(doc: Dict, results: List[str]) -> Dict:
         em = squad_metrics.compute_exact(gold_label, pred_label)
 
         return {"em": em}
-    return {"em": 0} 
-    
+    return {"em": 0}
+
+
+@register_filter("remove_whitespace_and_nones")
+class RemoveWhitespaceAndNones(Filter):
+
+    def apply(self, resps: list[list[str]], docs: list[dict]) -> list[list[str]]:
+        def filter_set(inst):
+            filtered_resp = []
+            for resp in inst:
+                if not resp:
+                    resp = ""
+                else:
+                    resp = resp.lstrip()
+                filtered_resp.append(resp)
+            return filtered_resp
+
+        filtered_resps = [filter_set(resp) for resp in resps]
+
+        return filtered_resps
