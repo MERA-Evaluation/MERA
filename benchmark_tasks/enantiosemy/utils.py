27603ff2d2ea8c9479b2614d89da7d3cdef1b744
mathamateur <sanya0112@mail.ru>
2026-06-16 10:53:39 +0300

    fix case in dataset path and add answer parser

--- benchmark_tasks/enantiosemy/utils.py
+++ benchmark_tasks/enantiosemy/utils.py
@@ -1,6 +1,6 @@
 from typing import Dict, List, Any
 from lm_eval.api.filter import Filter
-from lm_eval.api.registry import register_filter
+from lm_eval.api.registry import register_filter, FILTER_REGISTRY
 from transformers.data.metrics import squad_metrics
 
 
@@ -10,9 +10,12 @@ def doc_to_text(doc: Dict[str, Any]) -> str:
 
 
 def process_results(doc: Dict, results: List[str]) -> Dict:
+    
     if len(doc["outputs"]) > 0:
         gold_label = doc["outputs"]
         pred_label = results[0]
+        if "Ответ:" in pred_label:
+            pred_label = pred_label.split("Ответ:")[-1].strip()
 
         em = squad_metrics.compute_exact(gold_label, pred_label)
 
@@ -20,20 +23,21 @@ def process_results(doc: Dict, results: List[str]) -> Dict:
     return {"em": 0}
 
 
-@register_filter("remove_whitespace_and_nones")
-class RemoveWhitespaceAndNones(Filter):
+if "remove_whitespace_and_nones" not in FILTER_REGISTRY:
+    @register_filter("remove_whitespace_and_nones")
+    class RemoveWhitespaceAndNones(Filter):
 
-    def apply(self, resps: list[list[str]], docs: list[dict]) -> list[list[str]]:
-        def filter_set(inst):
-            filtered_resp = []
-            for resp in inst:
-                if not resp:
-                    resp = ""
-                else:
-                    resp = resp.lstrip()
-                filtered_resp.append(resp)
-            return filtered_resp
+        def apply(self, resps: list[list[str]], docs: list[dict]) -> list[list[str]]:
+            def filter_set(inst):
+                filtered_resp = []
+                for resp in inst:
+                    if not resp:
+                        resp = ""
+                    else:
+                        resp = resp.lstrip()
+                    filtered_resp.append(resp)
+                return filtered_resp
 
-        filtered_resps = [filter_set(resp) for resp in resps]
+            filtered_resps = [filter_set(resp) for resp in resps]
 
-        return filtered_resps
+            return filtered_resps
