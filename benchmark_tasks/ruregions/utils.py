d248b9515ba017ba9986a388fa948ec67522d5f2
mathamateur <sanya0112@mail.ru>
2026-06-23 22:05:38 +0300

    Remove redundant code

--- benchmark_tasks/ruregions/utils.py
+++ benchmark_tasks/ruregions/utils.py
@@ -1,11 +1,5 @@
 import os
-import sys
 from transformers.data.metrics import squad_metrics
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-if parent_dir not in sys.path:
-    sys.path.insert(0, parent_dir)
 
 
 def process_results(doc, results):
